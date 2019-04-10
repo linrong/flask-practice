6e8b92f1aa2e91c5a12dafe4f380c41dc7af6aeb
linrong <lazyhlr@gmail.com>
2019-04-10 11:22:24 +0800

    Refactor(file_hosting_system):修复数据库连接问题

--- file_hosting_system/config.py
+++ file_hosting_system/config.py
@@ -1,8 +1,8 @@
 DEBUG=True
 # SQLAlchmy ORM框架的方式使用数据库
-# dialect+driver://username:password@host:port/database
+# dialect+driver://username:password@host:port/database，目前host使用docker-compose网络中的mysql
 # dailect 数据库的实现，如MYSQL,driver是python对应的驱动，不指定则使用默认
-SQLALCHEMY_DATABASE_URI='mysql+pymysql://dgbuaa:dgbuaa@123.cn@localhost:3306/r'
+SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:dgbuaa@mysql:3306/flask'
 # 文件保存位置，要先创建
 UPLOAD_FOLDER='file'
 # 如果设置成 True (默认情况)，Flask-SQLAlchemy 将会追踪对象的修改并且发送信号。这需要额外的内存， 如果不必要的可以禁用它
