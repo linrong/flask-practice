1135c94abda9523cb74095d9dbbdc722ac663daa
linrong <lazyhlr@gmail.com>
2019-04-11 11:01:14 +0800

    Feat(flask-WTF):flask-WTF用于集成表单验证和渲染

---
+++ flask_demo/4/flask_wtf/config.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+DEBUG = True
+SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:dgbuaa@mysql:3306/flask'
+UPLOAD_FOLDER = '/tmp/permdir'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = 'a secret key'
