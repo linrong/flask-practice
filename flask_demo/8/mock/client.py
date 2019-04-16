91ec9d8219482562cb95a1df9ff846f9ca7ea9f1
linrong <lazyhlr@gmail.com>
2019-04-16 16:23:18 +0800

    Feat(mock):增加mock使用,主要用于模拟网络请求和数据库操作

---
+++ flask_demo/8/mock/client.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+import requests
+
+'''
+pip install requests
+'''
+def api_request(url):
+    r = requests.get(url)
+    return r.json()
+
+
+def get_review_author(url):
+    rs = api_request(url)
+    return rs['review']['author']
